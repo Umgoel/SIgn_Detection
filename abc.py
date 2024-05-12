8ad7b788fdf192379b67e8fef8af5e75bfbda7e7
Umang Goel <76695806+Umgoel@users.noreply.github.com>
2024-05-12 10:44:55 +0530

    5th sem project

---
+++ abc.py
@@ -0,0 +1,337 @@
+import streamlit as st
+import mediapipe as mp
+import cv2
+import numpy as np
+import tempfile
+import time
+import os
+from PIL import Image
+
+# import tensorflow as tf
+# from tensorflow.keras import layers
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+
+DEMO_VIDEO = 'demo.mp4'
+DEMO_IMAGE = 'demo.jpg'
+
+my_list = []
+
+
+
+
+st.markdown(
+    """
+    
+    """,
+    unsafe_allow_html=True,
+)
+
+st.sidebar.title('Sign Language Detection')
+st.sidebar.subheader('-Parameter')
+
+@st.cache()
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+
+    else:
+
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
+
+# (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+# train_images = train_images / 255.0
+# test_images = test_images / 255.0
+# model = tf.keras.Sequential([
+#     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Conv2D(64, (3, 3), activation='relu'),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Conv2D(64, (3, 3), activation='relu'),
+#     layers.Flatten(),
+#     layers.Dense(64, activation='relu'),
+#     layers.Dense(10)
+# ])
+# model.compile(optimizer='adam',
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#               metrics=['accuracy'])
+
+# history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+app_mode = st.sidebar.selectbox('Choose the App mode',
+['Sign Language to Text','Text to sign Language']
+)
+if app_mode == 'Sign Language to Text':
+    st.title('Sign Language to Text')
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+
+    use_webcam = st.sidebar.button('Use Webcam')
+    record = st.sidebar.checkbox("Record Video")
+    if record:
+        st.checkbox("Recording", value=True)
+
+    st.sidebar.markdown('---')
+    pw4=""
+    st.markdown(' ## Output')
+    st.markdown(pw4)
+
+    stframe = st.empty()
+    video_file_buffer = st.sidebar.file_uploader("Upload a video", type=["mp4", "mov", 'avi', 'asf', 'm4v'])
+    tfflie = tempfile.NamedTemporaryFile(delete=False)
+
+    if not video_file_buffer:
+        if use_webcam:
+            vid = cv2.VideoCapture(0)
+        else:
+            vid = cv2.VideoCapture(DEMO_VIDEO)
+            tfflie.name = DEMO_VIDEO
+
+    else:
+        tfflie.write(video_file_buffer.read())
+        vid = cv2.VideoCapture(tfflie.name)
+
+    vid = cv2.VideoCapture(0)
+
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps_input = int(vid.get(cv2.CAP_PROP_FPS))
+
+    codec = cv2.VideoWriter_fourcc('V', 'P', '0', '9')
+    out = cv2.VideoWriter('output1.mp4', codec, fps_input, (width, height))
+
+    st.markdown("", unsafe_allow_html=True)
+
+    st.sidebar.markdown('---')
+    st.markdown(
+        """
+        
+        """,
+        unsafe_allow_html=True,
+    )
+    finger_tips = [8, 12, 16, 20]
+    thumb_tip = 4
+    while True:
+        ret, img = vid.read()
+        img = cv2.flip(img, 1)
+        h, w, c = img.shape
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(img)
+
+        img.flags.writeable = True
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
+        if results.multi_hand_landmarks:
+            for hand_landmark in results.multi_hand_landmarks:
+                lm_list = []
+                for id, lm in enumerate(hand_landmark.landmark):
+                    lm_list.append(lm)
+                finger_fold_status = []
+                for tip in finger_tips:
+                    x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+                    # print(id, ":", x, y)
+                    # cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+
+                    if lm_list[tip].x < lm_list[tip - 2].x:
+                        # cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+                        finger_fold_status.append(True)
+                    else:
+                        finger_fold_status.append(False)
+
+                print(finger_fold_status)
+                x, y = int(lm_list[8].x * w), int(lm_list[8].y * h)
+                print(x, y)
+
+
+
+                # one
+                if lm_list[3].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[4].y < lm_list[
+                    12].y:
+                    cv2.putText(img, "ONE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("1")
+
+                # two
+                if lm_list[3].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
+                    cv2.putText(img, "TWO", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("2")
+                    pw4="two"
+                # three
+                if lm_list[2].x < lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
+                    cv2.putText(img, "THREE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("3")
+                    pw4="three"
+
+                # four
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[2].x < lm_list[8].x:
+                    cv2.putText(img, "FOUR", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("4")
+                    pw4="Four"
+
+                # five
+                if lm_list[2].x < lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "FIVE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("5")
+                    pw4="Five"
+                    # six
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "SIX", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("6")
+                    pw4="Six"
+                # SEVEN
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "SEVEN", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("7")
+                    pw4="Seven"
+                # EIGHT
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "EIGHT", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("8")
+                    pw4="Eight"
+                # NINE
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y > lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "NINE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("9")
+                    pw4="Nine"
+                # A
+                if lm_list[2].y > lm_list[4].y and lm_list[8].y > lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x and lm_list[4].y < lm_list[6].y:
+                    cv2.putText(img, "A", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("A")
+                # B
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[2].x > lm_list[8].x:
+                    cv2.putText(img, "B", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("B")
+                    pw4="B"
+                # c
+                if lm_list[2].x < lm_list[4].x and lm_list[8].x > lm_list[6].x and lm_list[12].x > lm_list[10].x and \
+                        lm_list[16].x > lm_list[14].x and lm_list[20].x > lm_list[18].x:
+                    cv2.putText(img, "C", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("C")
+                # d
+                if lm_list[3].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[4].y > lm_list[8].y:
+                    cv2.putText(img, "D", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("D")
+
+                # E
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y > lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x and lm_list[4].y > lm_list[6].y:
+                    cv2.putText(img, "E", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("E")
+            mp_draw.draw_landmarks(img, hand_landmark,
+                                   mp_hands.HAND_CONNECTIONS,
+                                   mp_draw.DrawingSpec((0, 0, 255), 6, 3),
+                                   mp_draw.DrawingSpec((0, 255, 0), 4, 2)
+                                   )
+            if record:
+
+                out.write(img)
+
+
+            frame = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
+            frame = image_resize(image=frame, width=640)
+            stframe.image(frame, channels='BGR', use_column_width=True)
+
+    st.text('Video Processed')
+
+    output_video = open('output1.mp4', 'rb')
+    out_bytes = output_video.read()
+    st.video(out_bytes)
+
+    vid.release()
+    out.release()
+else:
+    st.title('Text to Sign Language')
+
+
+    # define function to display sign language images
+    def display_images(text):
+        # get the file path of the images directory
+        img_dir = "images/"
+
+        # initialize variable to track image position
+        image_pos = st.empty()
+
+        # iterate through the text and display sign language images
+        for char in text:
+            if char.isalpha():
+                # display sign language image for the alphabet
+                img_path = os.path.join(img_dir, f"{char}.png")
+                img = Image.open(img_path)
+
+                # update the position of the image
+                image_pos.image(img, width=500)
+
+                # wait for 2 seconds before displaying the next image
+                time.sleep(1)
+
+                # remove the image
+                image_pos.empty()
+            elif char == ' ':
+                # display space image for space character
+                img_path = os.path.join(img_dir, "space.png")
+                img = Image.open(img_path)
+
+                # update the position of the image
+                image_pos.image(img, width=500)
+
+                # wait for 2 seconds before displaying the next image
+                time.sleep(1)
+
+                # remove the image
+                image_pos.empty()
+
+        # wait for 2 seconds before removing the last image
+        time.sleep(2)
+        image_pos.empty()
+
+
+    text = st.text_input("Enter text:")
+    # convert text to lowercase
+    text = text.lower()
+
+    # display sign language images
+    display_images(text)
